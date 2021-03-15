192a1490528fdb2c8ba78c3c471aeab171e3baa4
Abdulrahman <abdulrahman.qabbout@gmail.com>
2021-03-15 14:55:35 +0200

    initializing repository impl and test

---
+++ test/features/data/repositories/number_trivia_repository_impl_test.dart
@@ -0,0 +1,32 @@
+import 'package:clean_arch_tdd_trivia_app/core/platform/network_info.dart';
+import 'package:clean_arch_tdd_trivia_app/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:clean_arch_tdd_trivia_app/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:clean_arch_tdd_trivia_app/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:clean_arch_tdd_trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_arch_tdd_trivia_app/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockRemoteDataSource extends Mock
+    implements NumberTriviaRemoteDataSource {}
+
+class MockLocalDataSource extends Mock implements NumberTriviaLocalDataSource {}
+
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
+main(List<String> args) {
+  NumberTriviaRepositoryImpl repository;
+  MockRemoteDataSource mockRemoteDataSource;
+  MockLocalDataSource mockLocalDataSource;
+  MockNetworkInfo mockNetworkInfo;
+
+  setUp(() {
+    mockRemoteDataSource = MockRemoteDataSource();
+    mockLocalDataSource = MockLocalDataSource();
+    mockNetworkInfo = MockNetworkInfo();
+    repository = NumberTriviaRepositoryImpl(
+        remoteDataSource: mockRemoteDataSource,
+        localDataSource: mockLocalDataSource,
+        networkInfo: mockNetworkInfo);
+  });
+}
